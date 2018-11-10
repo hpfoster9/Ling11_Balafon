ca16b9160f72294e5db2c106bb9efa0f09c5ed66
Henry Foster <henrythefoster@gmail.com>
2018-11-09 19:53:37 -0500

    finished up logic and implimented rhythm

--- phrases.js
+++ phrases.js
@@ -2,31 +2,31 @@ phrases = [
   {
     "english": "Hello [to multiple people]",
     "seenku": "i kuna we",
-    "notes": [["D",1],["F",1],["A",0],["C",1],["F",0]]
+    "notes": [[1,["D",1]],[2,["F",1],["A",0]],[2,["C",1],["F",0]]]
   },
   {
     "english": "Has your morning been good?",
     "seenku": "a godageku ku aa",
-    "notes": [["F",0],["F",0],["F",0],["F",0],["F",0],["C",1],["A",0],["C",1], ["F",0]]
+    "notes": [[1,["F",0]],[2,["F",0],["F",0]],[2,["F",0],["F",0]],[2,["C",1],["A",0]],[2,["C",1], ["F",0]]]
   },
   {
     "english": "Is your wife good?",
     "seenku": "a mni ku aa",
-    "notes": [["F",0],["D",1],["D",1],["C",1],["A",0],["C",1], ["F",0]]
+    "notes": [[1,["F",0]],[2,["D",1],["D",1]],[2,["C",1],["A",0]],[2,["C",1], ["F",0]]]
   },
   {
     "english": "Are the children good?",
     "seenku": "a dronge ku aa",
-    "notes": [["F",0],["C",1],["D",1],["C",1],["A",0],["C",1],["F", 0]]
+    "notes": [[1,["F",0]],[2,["C",1],["D",1]],[2,["C",1],["A",0]],[2,["C",1],["F", 0]]]
   },
   {
     "english": "May God give you a good day",
     "seenku": "sumna la sonta weko",
-    "notes": [["D",1],["D",1],["D",1],["A",0],["F",0],["C",1],["C",1],["C",1],["C",1]]
+    "notes": [[1,["D",1]],[2,["D",1],["D",1]],[1,["A",0]],[1,["F",0]],[1,["C",1]],[2,["C",1],["C",1]],[1,["C",1]]]
   },
   {
     "english": "May God leave you near me",
     "seenku": "sumna la to mo kare",
-    "notes": [["F",1],["F",1],["F",1],["D",1],["A",0],["C",1],["F",0],["F",0]]
+    "notes": [[1,["F",1]],[2,["F",1],["F",1]],[1,["D",1]],[1,["A",0]],[1,["C",1]],[2,["F",0],["F",0]]]
   }
 ]
